7c1d3443c132c41f3e87e7f393f942d2537fc6e0
KnockoffofKnockoff <156373475+KnockoffofKnockoff@users.noreply.github.com>
2025-09-18 21:29:53 +0000

    cleaned up formatting and added missing pass keyword

--- robot.py
+++ robot.py
@@ -8,24 +8,21 @@ class MyRobot(wpilib.TimedRobot):
         This function is called upon program startup and
         should be used for any initialization code.
         """
-
         pass
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
-
         pass
     def autonomousPeriodic(self):
         """This function is called periodically during autonomous."""
-
         pass
 
     def teleopInit(self):
         """This function is called once each time the robot enters teleoperated mode."""
+        pass
 
     def teleopPeriodic(self):
         """This function is called periodically during teleoperated mode."""
-
         pass
 
     def testInit(self):
@@ -34,7 +31,6 @@ class MyRobot(wpilib.TimedRobot):
 
     def testPeriodic(self):
         """This function is called periodically during test mode."""
-
         pass
 
 
